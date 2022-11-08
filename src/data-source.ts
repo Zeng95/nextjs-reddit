0877240ea097abc392c7c64b77742edcaf5c0573
Leo Zeng <qingchun1995@gmail.com>
2022-11-08 17:17:31 +0800

    style: code formatting

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,17 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entities/User"
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { User } from './entities/User';
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "leozeng",
-    password: "root",
-    database: "reddit_development",
-    synchronize: true,
-    logging: true,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'leozeng',
+  password: 'root',
+  database: 'reddit_development',
+  synchronize: true,
+  logging: true,
+  entities: [User],
+  migrations: [],
+  subscribers: []
+});
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,24 @@
-import express from "express";
-import morgan from "morgan";
-import { AppDataSource } from "./data-source";
+import express from 'express';
+import morgan from 'morgan';
+import { AppDataSource } from './data-source';
+import authRoutes from './routes/auth';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.get('/', (_, res) => res.send('Hello World'));
+app.use('/api/auth', authRoutes);
 
 app.listen(port, async () => {
   console.log(`Server listening on port ${port}`);
 
   try {
-    await AppDataSource.initialize()
-    console.log("Database initialized and connected!");
+    await AppDataSource.initialize();
+    console.log('Database initialized and connected!');
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 });
