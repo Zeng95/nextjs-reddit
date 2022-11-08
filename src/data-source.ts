d1fe64754b62beb2a09caed672e9189ab1e702a6
Leo Zeng <qingchun1995@gmail.com>
2022-11-08 13:58:16 +0800

    chore: rename folders

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,14 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import { User } from "./entities/User"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
     port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
+    username: "leozeng",
+    password: "root",
+    database: "reddit_clone_development",
     synchronize: true,
     logging: false,
     entities: [User],
